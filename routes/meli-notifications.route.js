b9fcbf2c695835a6a7adec34c556340ba4bf0188
marcelo <pasutmarcelo@gmail.com>
2022-07-13 15:36:46 -0300

    fix response

--- routes/meli-notifications.route.js
+++ routes/meli-notifications.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.post('/',  (req, res, next) => {
   const noti = req.body;
   console.log(`New notification -> ${noti.topic}`);
+  res.status(201).send({ok: 'ok'});
 });
 
 module.exports = router;
