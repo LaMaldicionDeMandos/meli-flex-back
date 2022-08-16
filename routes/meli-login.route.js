fcf3779102b1d0bded9e2d259459498f2a9a0bc1
marcelo <pasutmarcelo@gmail.com>
2022-08-16 10:05:46 -0300

    implemento endpoint de access token para la app mobile

--- routes/meli-login.route.js
+++ routes/meli-login.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const MELI_URL = process.env.MELI_MOBILE_LOGIN_REDIRECT_URL;
+
 router.get('/',  (req, res, next) => {
-  res.redirect(`http://localhost:3000/login?code=${req.query.code}`);
+  res.redirect(`${MELI_URL}?code=${req.query.code}`);
 });
 
 module.exports = router;
--- services/authentication.service.js
+++ services/authentication.service.js
@@ -3,8 +3,11 @@ const redisService = require('./redis.service');
 
 
 const MELI_SECRET = process.env.MELI_SECRET;
+const MELI_MOBILE_SECRET = process.env.MELI_MOBILE_SECRET;
 const MELI_APP_ID = process.env.MELI_APP_ID;
+const MELI_MOBILE_APP_ID = process.env.MELI_MOBILE_APP_ID;
 const MELI_REDIRECT_URL = process.env.MELI_REDIRECT_URL;
+const MELI_MOBILE_REDIRECT_URL = process.env.MELI_MOBILE_REDIRECT_URL;
 const MELI_ACCESS_TOKEN_URL = 'https://api.mercadolibre.com/oauth/token';
 const GRANT_TYPE_CODE = 'authorization_code';
 const GRANT_TYPE_REFRESH = 'refresh_token';
@@ -46,6 +49,26 @@ class AuthenticationService {
       )
       .then(response => response.data)
       .then(saveUser);
+  }
+
+  requestMeliMobileAccessToken(code) {
+    return axios
+      .post(
+        `${MELI_ACCESS_TOKEN_URL}?grant_type=${GRANT_TYPE_CODE}&client_id=${MELI_MOBILE_APP_ID}&client_secret=${MELI_MOBILE_SECRET}&code=${code}&redirect_uri=${MELI_MOBILE_REDIRECT_URL}`,
+        {},
+        { headers: HEADERS }
+      )
+      .then(response => response.data);
+  }
+
+  refreshMeliMobileToken(refreshToken) {
+    return axios
+      .post(
+        `${MELI_ACCESS_TOKEN_URL}?grant_type=${GRANT_TYPE_REFRESH}&client_id=${MELI_MOBILE_APP_ID}&client_secret=${MELI_MOBILE_SECRET}&refresh_token=${refreshToken}`,
+        {},
+        { headers: HEADERS }
+      )
+      .then(response => response.data);
   }
 
   getUser(token) {
