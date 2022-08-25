210613715d5c25962eed49631969577c848f7267
marcelo <pasutmarcelo@gmail.com>
2022-08-25 17:56:50 -0300

    pruebo algo para cerrar la ventana

--- routes/meli-login.route.js
+++ routes/meli-login.route.js
@@ -6,7 +6,7 @@ const MELI_URL = process.env.MELI_MOBILE_LOGIN_REDIRECT_URL;
 router.get('/',  (req, res, next) => {
   const redirectUrl = `${MELI_URL}?code=${req.query.code}`;
   console.log('Redirecting to ' + redirectUrl);
-  res.redirect(redirectUrl);
+  res.send("<script type='text/javascript'>window.opener.location.href='**Your new url of new page after completing test**';self.close();</script>");
 });
 
 module.exports = router;
