8b73d5a1855799c9c12209ec1f8bc3977165ecc1
marcelo <pasutmarcelo@gmail.com>
2022-08-25 18:58:19 -0300

    redirect a app://?

--- routes/meli-login.route.js
+++ routes/meli-login.route.js
@@ -7,8 +7,9 @@ const MELI_URL = process.env.MELI_MOBILE_LOGIN_REDIRECT_URL;
 
 router.get('/',  (req, res, next) => {
   const redirectUrl = `${MELI_URL}?code=${req.query.code}`;
-  console.log('Redirecting to ' + redirectUrl);
-  res.sendFile(path.join(public, 'login_redirect.html'));
+  res.redirect('app://com.mpasut.happyshipping/login?code=' + req.query.code);
+  //console.log('Redirecting to ' + redirectUrl);
+  //res.sendFile(path.join(public, 'login_redirect.html'));
 });
 
 module.exports = router;
