fb31d6ca4a8bb685f1691b5c756ce284fd35ada6
marcelo <pasutmarcelo@gmail.com>
2022-08-25 19:08:37 -0300

    a ver ahhora?

--- routes/meli-login.route.js
+++ routes/meli-login.route.js
@@ -2,14 +2,19 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const public = path.join(__dirname, '../public');
+const deeplink = require('node-deeplink');
 
 const MELI_URL = process.env.MELI_MOBILE_LOGIN_REDIRECT_URL;
 
-router.get('/',  (req, res, next) => {
-  const redirectUrl = `${MELI_URL}?code=${req.query.code}`;
-  res.redirect('app://com.mpasut.happyshipping/login?code=' + req.query.code);
-  //console.log('Redirecting to ' + redirectUrl);
-  //res.sendFile(path.join(public, 'login_redirect.html'));
-});
+router.get('/',
+  (req, res, next) => {
+    const redirectUrl = `app://login?code=${req.query.code}`;
+    req.query['url'] = redirectUrl;
+    next();
+  },
+  deeplink({
+    fallback: MELI_URL,
+    android_package_name: 'com.mpasut.happyshipping'
+  }));
 
 module.exports = router;
